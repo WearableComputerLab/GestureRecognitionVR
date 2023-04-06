8f03aac2872e0a938d22d27b633f6d3b938b878f
Lewis Oborn <obolp001@mymail.unisa.edu.au>
2023-04-06 13:44:21 +0930

    Add Animator component to Hand Model
    Further develop outline of GesturePlayback script.

--- GestureRecognitionVR/Assets/GesturePlayback.cs
+++ GestureRecognitionVR/Assets/GesturePlayback.cs
@@ -6,17 +6,41 @@ public class GesturePlayback : MonoBehaviour
 {
     private GestureDetect gestureDetect;
     private Dictionary<string, Gesture> gestureList;
+    private List<string> gestureNames;
     private int currentGestureIndex;
+
+    // Add Animator Component to handModel 
     public GameObject handModel;
+    // Use Animator?
+    private Animator handAnimator;
+
+    // Need another model for left and right hand? or just flip model?
 
     private void Start()
     {
         gestureDetect = GetComponent<GestureDetect>();
+        handAnimator = handModel.GetComponent<Animator>();
+        gestureNames = new List<string>(gestureList.Keys);
         gestureList = GestureDetect.gestures;
     }
 
     private void Update()
     {
         
+    }
+
+    public void StartPlayback()
+    {
+
+    }
+
+    public void StopPlayback()
+    {
+
+    }
+
+    private void PlayGesture(string gestureName)
+    {
+
     }
 }
